334cc290d71a7599f25555eae1741291823ed0e9
Andros <wong22@hotmail.co.uk>
2017-11-07 15:53:51 -0800

    Finish milestone 2

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,31 @@
 "use strict";
 var express = require('express');
 var router = express.Router();
-
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 module.exports = function(passport, db) {
-  // YOUR CODE HERE
+  router.post('/register', (req, res) => {
+    console.log(req.body.username);
+    db.query('SELECT * from users WHERE username=$1', [req.body.username])
+      .then(result => {
+        if(result.rows.length !== 0) {
+          res.send('Username already taken!');
+        } else {
+          if(req.body.password !== req.body.password2) {
+            res.send("Passwords don't match");
+          } else {
+            bcrypt.hash(req.body.password, saltRounds)
+              .then(hash => {
+                return db.query('INSERT into users (username, password, email, phone_number, street_address, city, zipcode) VALUES ($1, $2, $3, $4, $5, $6, $7)', [req.body.username, hash, req.body.email, req.body.phone, req.body.street, req.body.city, req.body.postal]);
+              })
+              .then(() => {
+                res.redirect('/login');
+              });
+          }
+        }
 
+      });
+  });
+  router.post('/login', passport.authenticate('local', {successRedirect: '/dashboard', failureRedirect: '/login'}));
   return router;
-}
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 module.exports = function(db) {
   router.get('/', (req, res, next) => {
     res.render('index', {title: 'PokeBay'});
@@ -8,5 +9,54 @@ module.exports = function(db) {
 
   // YOUR CODE HERE
 
+  router.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  router.get('/register', (req, res) => {
+    res.render('register');
+  });
+
+  router.use((req, res, next) => {
+    if(!req.user) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  });
+
+  router.get('/dashboard', (req, res) => {
+    res.render('dashboard', {user: req.user});
+  });
+  router.get('/profile', (req, res) => {
+    res.render('profile', {user: req.user});
+  });
+  router.post('/profile/edit', (req, res) => {
+    console.log("REQ BODY", req.body, req.user.password);
+    bcrypt.compare(req.body.password, req.user.password)
+      .then(resp => {
+        if(!resp) {
+          res.render('profile', {user: req.user, error: 'Did not enter correct password'});
+        } else {
+          if(req.body.new_password) {
+            bcrypt.hash(req.body.new_password, saltRounds)
+              .then(hash => {
+                return db.query('UPDATE users set username=$1, password=$2, email=$3, street_address=$4, city=$5, zipcode=$6 WHERE username=$7', [req.body.username, hash, req.body.email, req.body.street_address, req.body.city, parseInt(req.body.zipcode), req.body.username]);
+              })
+              .then(result => {
+                console.log("result rows", result.rows);
+                res.render('profile', {user: result.rows[0]});
+              });
+          } else {
+            db.query('UPDATE users set username=$1, email=$3, street_address=$4, city=$5, zipcode=$6 WHERE username=$7', [req.body.username, req.body.email, req.body.street_address, req.body.city, parseInt(req.body.zipcode), req.body.username])
+              .then(result => {
+                res.render('profile', {user: result.rows[0]});
+              });
+          }
+
+        }
+      });
+
+  });
   return router;
-}
+};
