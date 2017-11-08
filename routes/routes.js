bb1f0bb3c21f12ba8c1d87ef54137e2cb0d6b48a
Emma Paschke <epaschke>
2017-11-07 17:01:25 -0800

    auction

--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,7 @@ module.exports = function(db) {
     if (! req.user) {
       res.redirect('/login');
     } else {
+      console.log(req.user)
       next();
     }
   });
@@ -19,12 +20,51 @@ module.exports = function(db) {
   });
 
   router.get('/auction/new', (req, res, next) => {
-    res.render('newAuction');
+    db.query('select * from pokemon')
+    .then(result => {
+      res.render('newAuction', {pokemon: result.rows});
+    })
+    .catch(err => {
+      console.log(err);
+    });
   });
 
   router.post('/auction/new', (req, res, next) => {
-    res.redirect('/dashboard');
+    db.query('insert into auctions (fk_users, fk_pokemon, opening_bid, reserve_price, duration, shipping_location, description, creation) values ($1, $2, $3, $4, $5, $6, $7, 00000)',
+    [req.user.id, req.body.pokemon, req.body.opening_bid, req.body.reserve_price, req.body.duration, req.body.shipping_location, req.body.description])
+    .then(() => {
+      console.log('created auction');
+      res.redirect(`/dashboard`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   });
+
+  router.get('/auction/:id', (req, res, next) => {
+    db.query('select * from auctions a join pokemon p on a.fk_pokemon = p.id where a.id = $1', [req.params.id])
+    .then(result => {
+      console.log(result);
+      if (req.user.id === result.rows[0].fk_users){
+        res.render('auction', { auction: result.rows[0], editable: true});
+      } else {
+        res.render('auction', { auction: result.rows[0]});
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  });
+
+  router.get('/delete/:auctionid', (req, res, next) => {
+    db.query('delete from auctions where id = $1', [req.params.auctionid])
+    .then(() => {
+      console.log('deleted');
+    })
+    .catch(err => {
+      console.log('Error: ', err);
+    })
+  })
 
   return router;
 };
