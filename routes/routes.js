e7cfcf0d2fe9d78e66bb6cda9669fcbf5aab9a91
jeff <tangj1122@gmail.com>
2017-11-07 16:59:15 -0800

    multiple SETs dont work

--- routes/routes.js
+++ routes/routes.js
@@ -1,49 +1,56 @@
 var express = require('express');
 var router = express.Router();
-var pool = require('../pool');
-var _ = require('underscore');
 
 module.exports = function(db) {
   router.get('/', (req, res, next) => {
     res.render('index', {title: 'PokeBay'});
   });
 
-  router.use((req, res, next) => {
-    if (! req.user) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  });
-
-  router.get('/dashboard', (req, res, next) => {
-    res.render('dashboard');
-  });
-
-  router.get('/auction/new', (req, res, next) => {
-    res.render('newAuction');
-  });
-
-  router.post('/auction/new', (req, res, next) => {
-    res.redirect('/dashboard');
-  });
+  // YOUR CODE HERE
 
   router.get('/profile', (req, res) => {
-    const requser = {first_name: 'jeff', last_name: 'tang', street_adress: '450 9th St.', city: 'San Francisco', state: 'CA', zipcode: 94109, username: 'tangsauce', password: 'baseball', email: 'jeff@gmail.com', phone: 5134263380};
+    // const requser = {first_name: 'jeff', last_name: 'tang', street_adress: '450 9th St.', city: 'San Francisco', state: 'CA', zipcode: 94109, username: 'tangsauce', password: 'baseball', email: 'jeff@gmail.com', phone: 5134263380};
     // const user = _.filter(requser, (val, key) => (key !== 'password'));
-    res.render('profile', {title: 'Profile', user: requser});
+    console.log(req.user.street_address); // only renders '450'
+    res.render('profile', {title: 'Profile', user: req.user});
   });
 
   router.post('/profile', (req, res) => {
-    // may pass in req.body.first_name
-    const query = {
-      text: 'SELECT * FROM users WHERE user=$1 SET',
-      values: [req.user.username, req.body.first_name, req.body.last_name, req.body.street_address, req.body.city, req.body.state, req.body.zipcode, req.body.username, req.body.password, req.body.email, req.body.phone] // what happens to req.user when db username is changed? do we have to login into MongoStore again / reserialize?
-    };
-    pool.query(query)
-    .then(resp => res.json({resp}))
-    .catch(err => res.json({err}));
+    // validate req.user.passwordSubmit before querying changes
+    if (req.body.passwordSubmit !== req.user.password) {
+      res.send('incorrect password');
+    } else {
+      const query = {
+        text: `UPDATE users
+                SET city=$2, SET street_address=$3
+                WHERE username=$1
+              `,
+        values: [req.user.username,
+                 req.body.city,
+                 req.body.street_address]
+                 // req.body.street_address]
+                 // req.body.city,
+                 // req.body.state,
+                 // parseInt(req.body.zipcode),
+                 // req.body.email,
+                 // parseInt(req.body.phone)]
+      };
+        // text: 'UPDATE users SET street_address=$4 WHERE username=$1',
+      console.log(query.values, req.user);
+      db.query(query)
+      .then(resp => res.json({resp}))
+      .catch(err => res.json({err}));
+    }
   });
 
+  // const query = {
+  //   text: 'SELECT * FROM users WHERE username=$1',
+  //   values: ['jeff']
+  // };
+  // db.query(query)
+  // .then(resp => console.log({resp: resp.rows[0]}))
+  // .catch(err => console.log({err}));
+  //
+  //
   return router;
 };
