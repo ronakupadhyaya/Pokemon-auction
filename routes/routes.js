1fc813c89d80b23c64fe4b1449132c5ed6f5ccdc
jeff <tangj1122@gmail.com>
2017-11-08 10:03:14 -0800

    fin profile edit

--- routes/routes.js
+++ routes/routes.js
@@ -6,9 +6,7 @@ module.exports = function(db) {
     res.render('index', {title: 'PokeBay'});
   });
 
-<<<<<<< HEAD
   // YOUR CODE HERE
-=======
   router.use((req, res, next) => {
     if (! req.user) {
       res.redirect('/login');
@@ -17,62 +15,34 @@ module.exports = function(db) {
       next();
     }
   });
->>>>>>> bb1f0bb3c21f12ba8c1d87ef54137e2cb0d6b48a
 
   router.get('/profile', (req, res) => {
-    // const requser = {first_name: 'jeff', last_name: 'tang', street_adress: '450 9th St.', city: 'San Francisco', state: 'CA', zipcode: 94109, username: 'tangsauce', password: 'baseball', email: 'jeff@gmail.com', phone: 5134263380};
-    // const user = _.filter(requser, (val, key) => (key !== 'password'));
-    console.log(req.user.street_address); // only renders '450'
     res.render('profile', {title: 'Profile', user: req.user});
   });
 
-<<<<<<< HEAD
   router.post('/profile', (req, res) => {
-    // validate req.user.passwordSubmit before querying changes
     if (req.body.passwordSubmit !== req.user.password) {
-      res.send('incorrect password');
+      res.render('profile', {title: 'Profile', user: req.user, error: 'incorrect password'});
     } else {
       const query = {
         text: `UPDATE users
-                SET city=$2, SET street_address=$3
+                SET username=$2, password=$3, street_address=$4, city=$5, state=$6, zipcode=$7, email=$8, phone=$9
                 WHERE username=$1
               `,
-        values: [req.user.username,
-                 req.body.city,
-                 req.body.street_address]
-                 // req.body.street_address]
-                 // req.body.city,
-                 // req.body.state,
-                 // parseInt(req.body.zipcode),
-                 // req.body.email,
-                 // parseInt(req.body.phone)]
+        values: [req.user.username, // 1
+                 req.body.username, // 2
+                 req.body.password, // 3
+                 req.body.street_address, // 4
+                 req.body.city, // 5
+                 req.body.state, // 6
+                 parseInt(req.body.zipcode), // 7
+                 req.body.email, // 8
+                 parseInt(req.body.phone)] // 9
       };
-        // text: 'UPDATE users SET street_address=$4 WHERE username=$1',
-      console.log(query.values, req.user);
       db.query(query)
       .then(resp => res.json({resp}))
       .catch(err => res.json({err}));
     }
-  });
-
-  // const query = {
-  //   text: 'SELECT * FROM users WHERE username=$1',
-  //   values: ['jeff']
-  // };
-  // db.query(query)
-  // .then(resp => console.log({resp: resp.rows[0]}))
-  // .catch(err => console.log({err}));
-  //
-  //
-=======
-  router.get('/auction/new', (req, res, next) => {
-    db.query('select * from pokemon')
-    .then(result => {
-      res.render('newAuction', {pokemon: result.rows});
-    })
-    .catch(err => {
-      console.log(err);
-    });
   });
 
   router.post('/auction/new', (req, res, next) => {
@@ -112,6 +82,5 @@ module.exports = function(db) {
     })
   })
 
->>>>>>> bb1f0bb3c21f12ba8c1d87ef54137e2cb0d6b48a
   return router;
 };
